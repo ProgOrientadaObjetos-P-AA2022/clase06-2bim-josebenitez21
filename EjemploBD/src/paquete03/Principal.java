be437635e0bb102f5d4bd284a6e792fda7f289a9
Jose Benitez <jlbenitez4@utpl.edu.ec>
2022-07-15 11:47:33 -0500

    Cambios en clase

--- EjemploBD/src/paquete03/Principal.java
+++ EjemploBD/src/paquete03/Principal.java
@@ -14,12 +14,12 @@ import paquete02.Ciudad;
  * @author reroes
  */
 public class Principal {
-    
+
     public static void main(String[] args) throws SQLException {
         Enlace c = new Enlace();
         Ciudad ciudad = new Ciudad("Cuenca", 100123);
-        c.insertarCiudad(ciudad);
-        
+       // c.insertarCiudad(ciudad);
+
         for (int i = 0; i < c.obtenerDataCiudad().size(); i++) {
             System.out.printf("%s", c.obtenerDataCiudad().get(i));
         }
